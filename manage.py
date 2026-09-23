659b42c2716820a63b236d034f8eb0f6ca322d7d
CharlesMonjakidy <charlesmonjakidy@gmail.com>
2026-09-23 23:01:18 +0300

    Fix Django settings for Vercel

--- manage.py
+++ manage.py
@@ -2,10 +2,14 @@
 import os
 import sys
 
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "devai.settings")
+
+from django.core.management import execute_from_command_line
+
+
 def main():
-    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "devai.settings")
-    from django.core.management import execute_from_command_line
     execute_from_command_line(sys.argv)
+
 
 if __name__ == "__main__":
     main()
